32386e8b593ee39fd97f73f6d743fc5b9e028e31
Andre <andrevitoreur@gmail.com>
2023-04-20 12:22:20 +0100

    populate cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import RepoCard from "@/components/RepoCard"
 import MostCards from "@/components/MostCards"
 import TrendingCards from "@/components/TrendingCards"
 
-export default function Home() {
+export default function Home() {  
   return (
     <main className="flex flex-col gap-2 bg-white items-center">
         <Search/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+        'hero-pattern': "url('/me.jpg')"
+        },
     },
   },
   plugins: [],
